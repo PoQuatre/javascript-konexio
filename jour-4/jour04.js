8acd4727fff22a6857ea9a2ce55a6bdcc8ab44c9
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-27 14:41:48 +0100

    Added day 4 exercise 3

--- jour-4/jour04.js
+++ jour-4/jour04.js
@@ -19,3 +19,16 @@ let cats = [cat, cat2];
 
 console.log(cats[0].age);
 console.log(cats[1].isCute);
+
+// Exercice 3
+function checkIfEven(num) {
+  console.log(num, "is", num % 2 === 0 ? "even" : "odd");
+}
+
+checkIfEven(0);
+checkIfEven(93);
+checkIfEven(-23);
+checkIfEven(-22);
+checkIfEven(12);
+checkIfEven(47);
+checkIfEven(58);
