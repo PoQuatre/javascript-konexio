879cd5b10c0cb64d784417b00fcda3f49cc2a6a3
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-27 13:50:11 +0100

    Added day 3 exercise 3

--- jour-3/jour03.js
+++ jour-3/jour03.js
@@ -7,3 +7,18 @@ console.table(fruits);
 let ingredients = ["eggs", "milk", "butter"];
 console.log(ingredients[1]);
 console.log(ingredients.indexOf("butter"));
+
+// Exercice 3
+let objects = ["pen", "book", "lamp"];
+
+objects.unshift("chair");
+console.log(objects);
+
+objects.pop();
+console.log(objects);
+
+objects.push("laptop");
+console.log(objects);
+
+objects.shift();
+console.log(objects);
