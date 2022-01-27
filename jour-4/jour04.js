1b360fa36b78f2eb8c16c366052a3e998fc18544
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-27 14:38:38 +0100

    Added day 4 exercise 2

--- jour-4/jour04.js
+++ jour-4/jour04.js
@@ -8,3 +8,14 @@ let cat = {
 console.log(cat);
 console.log(cat.age);
 if (cat.isCute) console.log("So cute !");
+
+// Exercice 2
+let cat2 = {
+  name: "Ficelle",
+  age: 12,
+  isCute: true,
+};
+let cats = [cat, cat2];
+
+console.log(cats[0].age);
+console.log(cats[1].isCute);
