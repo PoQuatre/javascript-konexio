baa9452d2526acec20194db4df1fa85d0e23165d
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-27 13:45:35 +0100

    Added day 3 exercise 2

--- jour-3/jour03.js
+++ jour-3/jour03.js
@@ -2,3 +2,8 @@
 let fruits = ["mango", "lemon", "blueberry"];
 console.log(fruits);
 console.table(fruits);
+
+// Exercice 2
+let ingredients = ["eggs", "milk", "butter"];
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
