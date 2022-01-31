f56977c03992af17ce16b83b4c135562c71e7ff8
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-31 14:34:30 +0100

    Added while loops exercise 2

--- loops/whileLoop.js
+++ loops/whileLoop.js
@@ -4,3 +4,14 @@ while (i <= 200) {
   if (i % 2 === 0) console.log("Exercice 1 -->", i);
   i++;
 }
+
+// Exercice 2
+let dice = null;
+let count = 0;
+
+while (dice !== 6) {
+  dice = Math.ceil(Math.random() * 6);
+  count++;
+}
+
+console.log("Exercice 2 -->", count);
