bdfc8068fb95b2251b5266a50ec3a9956ac4b83e
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-31 14:13:43 +0100

    Added for loops exercise 2

--- loops/forLoop.js
+++ loops/forLoop.js
@@ -4,3 +4,10 @@ for (let i = 5; i <= 10; i++) {
   result += i * i;
 }
 console.log("Exercice 1 -->", result);
+
+// Exercice 2
+result = 0;
+for (let i = 100; i <= 1000; i++) {
+  if (i % 7 === 0) result++;
+}
+console.log("Exercice 2 -->", result);
