a69ff7aa10ac0419ed47cc1d949a1c1bd00153f6
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-31 14:57:38 +0100

    Added hard loops exercise 2

--- loops/hardLoop.js
+++ loops/hardLoop.js
@@ -11,3 +11,17 @@ for (let i = 0; i < numbers.length; i++) {
 }
 
 console.log("Exercice 1 -->", sum);
+
+// Exercice 2
+numbers = [];
+let max = 0;
+
+for (let i = 0; i < 50; i++) {
+  numbers.push(Math.floor(Math.random() * 151) + 50);
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > max) max = numbers[i];
+}
+
+console.log("Exercice 2 -->", max);
