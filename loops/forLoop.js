d80b67416f9d9f9ae51d78b9a1794970ace14038
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-31 14:13:58 +0100

    Added for loops exercise 3

--- loops/forLoop.js
+++ loops/forLoop.js
@@ -11,3 +11,15 @@ for (let i = 100; i <= 1000; i++) {
   if (i % 7 === 0) result++;
 }
 console.log("Exercice 2 -->", result);
+
+// Exercice 3
+result = 0;
+
+const throwDice = (faces) => Math.ceil(Math.random() * faces);
+
+for (let i = 0; i < 20; i++) {
+  let value = throwDice(6);
+  if (value >= 5) result += value;
+}
+
+console.log("Exercice 3 -->", result);
