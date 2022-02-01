732e68e9647881bbc3cafff65ed06b24d5d6644e
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-01 14:38:49 +0100

    Added day 5 exercise 1

---
+++ jour-5/jour05.js
@@ -0,0 +1,34 @@
+const args = process.argv.slice(2);
+
+// Exercice 1
+function calculate(num1, symbol, num2) {
+  if (!num1 || !symbol || !num2) {
+    console.log("error");
+    return;
+  }
+
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+
+  switch (symbol) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "x":
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    case "%":
+      return num1 % num2;
+    default:
+      console.log("error");
+  }
+}
+
+console.log("Exercice 1 -->", calculate("5", "x", "4"));
+console.log(
+  "Exercice 1 -->",
+  calculate(args.shift(), args.shift(), args.shift())
+);
