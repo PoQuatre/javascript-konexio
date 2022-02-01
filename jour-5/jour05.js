49ab659ddd672aaba0299034fb94445ce697a9ce
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-01 14:41:58 +0100

    Added day 5 exercise 2

--- jour-5/jour05.js
+++ jour-5/jour05.js
@@ -32,3 +32,14 @@ console.log(
   "Exercice 1 -->",
   calculate(args.shift(), args.shift(), args.shift())
 );
+
+// Exercice 2
+function multiply(num) {
+  num = parseInt(num);
+  console.log("Exercice 2 --> Table de multiplication de", num);
+  for (let i = 1; i <= 10; i++) {
+    console.log("Exercice 2 -->", num, "x", i, "=", num * i);
+  }
+}
+
+multiply(args.shift());
