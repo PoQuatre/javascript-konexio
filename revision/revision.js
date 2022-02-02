25d8ca8f382ceb0c522a1063ff64cb2bec0a6716
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-02 11:58:53 +0100

    Added revision exercise 2

--- revision/revision.js
+++ revision/revision.js
@@ -3,4 +3,29 @@ function sortLetters(str) {
   return str.split("").sort().join("");
 }
 
-console.log(sortLetters("konexio"));
+console.log("Exercice 1 -->", sortLetters("konexio"));
+
+// Exercice 2
+function countEach(str) {
+  let x = 0;
+  let o = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i].toLowerCase()) {
+      case "x":
+        x++;
+        break;
+      case "o":
+        o++;
+        break;
+      default:
+        console.log(str[i], "is not supported");
+        break;
+    }
+  }
+
+  return x === o;
+}
+
+console.log("Exercice 2 -->", countEach("xxxoooox"));
+console.log("Exercice 2 -->", countEach("xox"));
