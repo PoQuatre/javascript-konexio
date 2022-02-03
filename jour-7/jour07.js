7c5a1fb2387d7fb66cd73f563ea7961e6804e769
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-03 14:15:39 +0100

    Added day 7 exercise 6

--- jour-7/jour07.js
+++ jour-7/jour07.js
@@ -38,3 +38,40 @@ console.log("Exercice 4 -->", numbers);
 const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8];
 const even = numbers2.filter((x) => x % 2 === 0);
 console.log("Exercice 5 -->", even);
+
+// Exercice 6
+const cakes = [
+  {
+    name: "cake",
+    flavor: "vanilla",
+    status: "available",
+  },
+  {
+    name: "brownie",
+    flavor: "chocolate",
+    status: "available",
+  },
+  {
+    name: "pie",
+    flavor: "strawberry",
+    status: "available",
+  },
+  {
+    name: "muffin",
+    flavor: "pistachio",
+    status: "available",
+  },
+  {
+    name: "donut",
+    flavor: "chocolate",
+    status: "available",
+  },
+];
+const chocolateCakes = cakes
+  .filter((cake) => cake.flavor === "chocolate")
+  .map((cake) => {
+    cake.status = "sold out !";
+    return cake;
+  });
+
+console.log(chocolateCakes);
