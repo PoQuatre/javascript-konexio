e0553b8febb487ef412a0caa5a1bb8b433189e5e
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-03 13:58:11 +0100

    Added day 7 exercise 2

--- jour-7/jour07.js
+++ jour-7/jour07.js
@@ -4,3 +4,8 @@ const path = require("path");
 
 const content = fs.readFileSync(path.join(__dirname, "jour07.txt"), "utf-8");
 console.log(content);
+
+// Exercice 2
+const array = [1, 2, 3, 4, 5];
+const double = array.map((x) => x * 2);
+console.log("Exercice 2 -->", double);
