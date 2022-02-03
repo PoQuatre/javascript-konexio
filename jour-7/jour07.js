8957b84fa67e62ee38da009e4e87b711ba93be3f
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-03 14:06:35 +0100

    Added day 7 exercise 3

--- jour-7/jour07.js
+++ jour-7/jour07.js
@@ -9,3 +9,22 @@ console.log(content);
 const array = [1, 2, 3, 4, 5];
 const double = array.map((x) => x * 2);
 console.log("Exercice 2 -->", double);
+
+// Exercice 3
+const longNames = [
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+  },
+  {
+    firstName: "John",
+    lastName: "Smith",
+  },
+];
+const shortNames = longNames.map((person) => {
+  return {
+    name: `${person.firstName} ${person.lastName}`,
+  };
+});
+
+console.log("Exercice 3 -->", shortNames);
