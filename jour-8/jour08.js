96522c66e5e7e0869db5b99dc8acf0ed25ccc8f2
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-07 14:49:15 +0100

    Added day 8 exercise 2

--- jour-8/jour08.js
+++ jour-8/jour08.js
@@ -16,3 +16,13 @@ const getCountries = (err, res, body) => {
 };
 
 request.get("https://restcountries.com/v3.1/all", getCountries);
+
+// Exercice 2
+const getFact = () => {
+  request.get("https://api.chucknorris.io/jokes/random", (err, res, body) => {
+    const data = JSON.parse(body);
+    console.log("Exercice 2 -->", data.value);
+  });
+};
+
+getFact();
